905a45e3cf0f26939a756b43671c10e3e4c20b63
eyad amr <eyadamro905@gmail.com>
2024-02-14 14:53:14 +0200

    1st

---
+++ lib/constants/colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+const Color tdRed =Color(0xffda4040);
+const Color tdBlue =Color(0xff5f52EE);
+
+const Color tdBlack =Color(0xff3a3a3a3a);
+const Color tdGrey =Color.fromARGB(112, 98, 96, 96);
+
+const Color tdBGColor =Color.fromARGB(255, 249, 249, 249);
